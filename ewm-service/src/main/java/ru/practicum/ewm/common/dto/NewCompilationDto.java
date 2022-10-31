6ef601bacea21fd516af48b5eb3d408960357a14
ArsenyWork <ArsenyRita@yandex.ru>
2022-10-31 18:56:17 +0300

    fix: - correction of remarks

--- ewm-service/src/main/java/ru/practicum/ewm/common/dto/NewCompilationDto.java
+++ ewm-service/src/main/java/ru/practicum/ewm/common/dto/NewCompilationDto.java
@@ -1,10 +1,12 @@
 package ru.practicum.ewm.common.dto;
 
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @Getter
--- ewm-service/src/main/java/ru/practicum/ewm/common/repository/RequestRepository.java
+++ ewm-service/src/main/java/ru/practicum/ewm/common/repository/RequestRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.common.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.ewm.common.enums.State;
-import ru.practicum.ewm.common.model.Event;
 import ru.practicum.ewm.common.model.Request;
 
 import java.util.List;
