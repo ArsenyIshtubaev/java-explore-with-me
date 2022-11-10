ab58b4fe59c08cc2abac2f75bd426bead75d94b7
ArsenyWork <ArsenyRita@yandex.ru>
2022-11-10 11:46:11 +0300

    fix: - correct validation

--- ewm-service/src/main/java/ru/practicum/ewm/client/HitClient.java
+++ ewm-service/src/main/java/ru/practicum/ewm/client/HitClient.java
@@ -16,7 +16,7 @@ import java.util.Map;
 public class HitClient extends BaseClient {
 
     @Autowired
-    public HitClient(@Value("http://localhost:9090") String serverUrl, RestTemplateBuilder builder) {
+    public HitClient(@Value("http://stats-server:9090") String serverUrl, RestTemplateBuilder builder) {
 
         super(
                 builder
